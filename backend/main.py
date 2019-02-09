9cb05db197082677b5f809e4e74ddb6663e07fbe
Owen Hughes <okayhughes@gmail.com>
2019-02-09 02:06:53 -0500

    Create post requests for schedules

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,50 @@
-from flask import Flask
+#Flask imports
+from flask import Flask, jsonify, request
 app = Flask(__name__)
+
+
+import pickledb
+
+db = pickledb.load('FEEB.db', False)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+#Getting and posting json
+# import requests
+# res = requests.post('http://localhost:5000/api/add_message/1234', json={"mytext":"lalala"})
+# if res.ok:
+#     print res.json()
+
+# from flask import Flask, request, jsonify
+# app = Flask(__name__)
+
+@app.route('/api/add_schedule/<schedule_id>', methods=['GET', 'POST'])
+def add_schedule(schedule_id):
+    if request.method == 'POST':
+        content = request.json
+        db.set('schedule_{}'.format(schedule_id), content)
+        return jsonify({"sid":schedule_id})
+    else:
+        content = db.get('schedule_{}'.format(schedule_id))
+        return jsonify(content)
+
+
+
+
+#Get and post at the same time:
+# def register():
+#     if flask.request.method == 'POST':
+#         username = flask.request.values.get('user') # Your form's
+#         password = flask.request.values.get('pass') # input names
+#         your_register_routine(username, password)
+#     else:
+#         # You probably don't have args at this route with GET
+#         # method, but if you do, you can access them like so:
+#         yourarg = flask.request.args.get('argname')
+#         your_register_template_rendering(yourarg)
+
+
+if __name__ == '__main__':
+    app.run(host= '0.0.0.0',debug=True)
